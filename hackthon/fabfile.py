45d473d97a528913b7e0299f49eae63b0d97a706
zaebee <zaebuntu@gmail.com>
2013-03-31 21:51:20 +0400

    changed fabric file

--- hackthon/fabfile.py
+++ hackthon/fabfile.py
@@ -9,14 +9,16 @@ def update():
         run('git pull')
 
 
-def status():
-    run('sudo supervisorctl status')
-
 def req():
     with cd('/home/zaebee/projects/hackpoint'):
         run('.env/bin/pip install -r hackthon/req.txt')
 
 
+def status():
+    run('sudo supervisorctl status')
+
+
 def upgrade():
     update()
+    req()
     status()
