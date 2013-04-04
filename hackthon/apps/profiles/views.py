3a2ff89c300f61825fe164adc7f3ddaf083b47c3
zaebee <zaebuntu@gmail.com>
2013-04-04 06:16:34 +0400

    added views for CRUD projects

--- hackthon/apps/profiles/views.py
+++ hackthon/apps/profiles/views.py
@@ -11,10 +11,11 @@ from django.views.decorators.csrf import csrf_exempt
 
 from django.shortcuts import get_object_or_404, render, redirect
 from django.conf import settings
+from django.contrib import messages
 
 #from django.http import HttpResponse
 from django.views.generic import ListView, DetailView
-from django.views.generic.edit import FormView, UpdateView
+from django.views.generic.edit import FormView, UpdateView, CreateView
 
 if "mailer" in settings.INSTALLED_APPS:
     from mailer import send_mail, send_html_mail
@@ -23,8 +24,8 @@ else:
 
 from annoying.decorators import ajax_request
 
-from profiles.forms import UserProfileForm, RegistrationFormUniqueEmail, SponsorshipForm
-from profiles.models import UserProfile
+from profiles.forms import UserProfileForm, RegistrationFormUniqueEmail, SponsorshipForm, UserProjectForm
+from profiles.models import UserProfile, UserProject
 
 
 class ProifileList(ListView):
@@ -72,6 +73,95 @@ class ProfileEditView(UpdateView):
                 'instance': self.userprofile
             })
         return kwargs
+
+    def form_valid(self, form):
+        messages.success(self.request, u'Ваш профиль успешно обновлен.')
+        return super(ProfileEditView, self).form_valid(form)
+
+
+class ProjectList(ListView):
+    queryset = UserProject.objects.all()
+    context_object_name = 'project_list'
+
+    @method_decorator(login_required)
+    def get(self, request, *args, **kwargs):
+        return super(ProjectList, self).get(request, *args, **kwargs)
+
+
+class ProjectDetail(DetailView):
+    context_object_name = 'project'
+    queryset = UserProject.objects.all()
+
+    @method_decorator(login_required)
+    def get(self, request, *args, **kwargs):
+        return super(ProjectDetail, self).get(request, *args, **kwargs)
+
+
+class ProjectEditView(UpdateView):
+    form_class = UserProjectForm
+    model = UserProject
+    template_name = 'profiles/edit_project.html'
+
+    @method_decorator(login_required)
+    def get(self, request, *args, **kwargs):
+        self.userproject = UserProject.objects.get(owner=request.user)
+        return super(ProjectEditView, self).get(request, *args, **kwargs)
+
+    @method_decorator(login_required)
+    def post(self, request, *args, **kwargs):
+        self.userproject = UserProject.objects.get(owner=request.user)
+        return super(ProjectEditView, self).post(request, *args, **kwargs)
+
+    def get_form_kwargs(self):
+        """
+        Возвращает словарь аргументов для экземпляра формы
+        """
+        kw = super(ProjectEditView, self).get_form_kwargs()
+        kwargs = {'initial': self.get_initial()}
+        if self.request.method in ('POST', 'PUT'):
+            kwargs.update({
+                'data': self.request.POST,
+                'files': self.request.FILES,
+                'instance': self.userproject
+            })
+        if self.request.method == 'GET':
+            kwargs.update({
+                'instance': self.userproject
+            })
+        return kwargs
+
+    def form_valid(self, form):
+        messages.success(self.request, u'Ваш проект успешно обновлен.')
+        return super(ProjectEditView, self).form_valid(form)
+
+
+class ProjectCreateView(CreateView):
+    model = UserProject
+    form_class = UserProjectForm
+    template_name = 'profiles/create_project.html'
+    success_url = '/accounts/projects/list/'
+
+    def dispatch(self, *args, **kwargs):
+        as_member = UserProject.objects.filter(
+            members=self.request.user.profile
+        ).exists()
+        as_owner = UserProject.objects.filter(
+            owner=self.request.user.profile
+        ).exists()
+        if as_member:
+            messages.success(self.request, u'Вы уже присоединились к проекту. Вы должны сначала покинуть проект, чтобы создать новый.', 'alert')
+        if as_owner:
+            messages.success(self.request, u'Вы можете создать только один проект. И он уже создан.', 'alert')
+        if as_owner or as_member:
+            return redirect('project_list')
+        return super(ProjectCreateView, self).dispatch(*args, **kwargs)
+
+    def form_valid(self, form):
+        self.object = form.save(commit=False)
+        self.object.owner = self.request.user.profile
+        self.object.save()
+
+        return redirect(self.get_success_url())
 
 
 @ajax_request
